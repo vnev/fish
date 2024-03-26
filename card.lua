c634e80e8d71b6ab7b5e6e27ef522772c557a204
Abhaya Basnet <basnetabhaya@gmail.com>
2024-03-26 00:52:35 -0400

    card class

--- card.lua
+++ card.lua
@@ -1,4 +1,6 @@
 local class = require 'middleclass'
+local love = require 'love'
+local AssetLoader = require "asset_loader"
 
 local Card = class('Card')
 
@@ -8,6 +10,16 @@ function Card:initialize(suit, rank, value, sub)
     self.id = suit .. rank
     self.value = value
     self.subdeck = suit .. '_' .. sub
+end
+
+function Card:draw(x, y, cardimages)
+    local image = cardimages[self.id]
+    if image then
+        love.graphics.draw(image, x, y)
+    end
+end
+
+function Card:update()
 end
 
 function Card:__tostring()
